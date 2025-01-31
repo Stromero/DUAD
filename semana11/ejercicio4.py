977625a0f5aa5437bdb49043cc97a473951d6f2a
Stromero <steven.romero.morera@gmail.com>
2025-01-31 15:38:11 -0600

    Changes on exercise as per coach request done

--- semana11/ejercicio4.py
+++ semana11/ejercicio4.py
@@ -13,29 +13,35 @@ class Feet:
 class Leg:
     def __init__(self, feet_parameter):
         self.feet = feet_parameter
-        pass
+        
 
 class Arm:
     def __init__(self, hand_parameter):
         self.hand = hand_parameter
-        pass
+        
 
 class Torso:
-    def __init__(self, head_parameter, leg_parameter, arm_parameter):
+    def __init__(self, head_parameter, right_leg_parameter, left_leg_parameter, right_arm_parameter, left_arm_parameter):
         self.head = head_parameter
-        self.leg = leg_parameter
-        self.arm = arm_parameter
-        pass
+        self.right_hand = right_arm_parameter
+        self.left_hand = left_arm_parameter
+        self.right_leg = right_leg_parameter
+        self.left_leg = left_leg_parameter
+        
 
 class Human:
     def __init__(self, torso_parameter):
         self.torso = torso_parameter
-        pass
+        
 
 main_head = Head()
 right_hand = Hand()
+left_hand = Hand()
 right_feet = Feet()
+left_feet = Feet()
 right_leg = Leg(right_feet)
+left_leg = Leg(left_feet)
 right_arm = Arm(right_hand)
-main_torso = Torso(main_head,right_leg,right_hand)
-body = Human = (main_torso)
+left_arm = Arm(left_hand)
+main_torso = Torso(main_head,right_leg, left_leg, right_arm,left_arm)
+body = Human(main_torso)
